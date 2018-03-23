ea1d7700e4474a52507d4a7c81b4cf7b6934e814
unknown <distancein1997@gmail.com>
2018-03-23 22:06:53 +0800

    add doctest

--- Sorting algorithms/BubbleSort.py
+++ Sorting algorithms/BubbleSort.py
@@ -3,6 +3,14 @@ from random import random
 a = [int(30 * random()) for i in range(30)]
 
 def bubblesort(a):
+    """Sort the list a to a assend order
+    
+    >>> bubblesort([2, 1, 4])
+    [1, 2, 4]
+    >>> bubblesort([1])
+    [1]
+    """
+
     n = len(a)
     for i in range(n - 1):
         for j in range(n - 1 - i):

2499cb1e12d2e4d32db1be17e31e45562a33f0bb
unknown <distancein1997@gmail.com>
2018-03-23 21:51:07 +0800

    bubble sort completed

---
+++ Sorting algorithms/BubbleSort.py
@@ -0,0 +1,11 @@
+from random import random
+
+a = [int(30 * random()) for i in range(30)]
+
+def bubblesort(a):
+    n = len(a)
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+    return a
